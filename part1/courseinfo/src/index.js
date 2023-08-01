4ea37e509735a67422b3c6bed2f116cd6b9dc0e9
Carmen-Rosas <carmen.rosas.cancio.suarez@gmail.com>
2023-08-01 16:50:56 +0200

    Update index.js

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Title = ({ course }) => <h1> {course} </h1>
-const Parragraph = ({ part, exercises }) => <p> {part} {exercises} </p>
-const Exercise = ({ exercises }) => <p> Number of exercises {exercises} </p>
+const Header = ({ course }) => <h1> {course} </h1>
+const Content = ({ part, exercises }) => <p> {part} {exercises} </p>
+const Total = ({ exercises }) => <p> Number of exercises {exercises} </p>
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -16,18 +16,18 @@ const App = () => {
 
   return (
     <div>
-      <Title course={course} />
+      <Header course={course} />
       <p>
-        <Parragraph part={part1} exercises={exercises1} />
+        <Content part={part1} exercises={exercises1} />
       </p>
       <p>
-        <Parragraph part={part2} exercises={exercises2} />
+        <Content part={part2} exercises={exercises2} />
       </p>
       <p>
-        <Parragraph part={part3} exercises={exercises3} />
+        <Content part={part3} exercises={exercises3} />
       </p>
       <p>
-        <Exercise exercises={exercises1 + exercises2 + exercises3} />
+        <Total exercises={exercises1 + exercises2 + exercises3} />
       </p>
     </div>
   )
