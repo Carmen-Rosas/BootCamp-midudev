11deee1e9b64a5d0e7cbbe1ebcf1f05e03d23b99
Carmen-Rosas <carmen.rosas.cancio.suarez@gmail.com>
2023-08-01 19:36:10 +0200

    Update index.js

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,33 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = ({ course }) => <h1> {course} </h1>
-const Content = ({ part, exercises }) => <p> {part} {exercises} </p>
-const Total = ({ exercises }) => <p> Number of exercises {exercises} </p>
+const Header = (props) => {
+  return <h1> {props.course} </h1>
+}
+
+const Content = (props) => {
+    return <p> {props.part} {props.exercises} </p>
+}
+
+const Total = (props) => {
+    return <p> Number of exercises {props.exercises} </p>
+}
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
       <p>
-        <Content part={part1} exercises={exercises1} />
+        <Content part={course.parts[0].name} exercises={course.parts[0].exercises} />
       </p>
       <p>
-        <Content part={part2} exercises={exercises2} />
+        <Content part={course.parts[1].name} exercises={course.parts[1].exercises} />
       </p>
       <p>
-        <Content part={part3} exercises={exercises3} />
+        <Content part={course.parts[2].name} exercises={course.parts[2].exercises} />
       </p>
       <p>
-        <Total exercises={exercises1 + exercises2 + exercises3} />
+        <Total exercises={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
       </p>
     </div>
   )
