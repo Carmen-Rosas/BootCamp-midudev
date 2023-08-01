2ee1a90936f97d93b6bf1c952b39b431a6338c05
Carmen-Rosas <carmen.rosas.cancio.suarez@gmail.com>
2023-08-01 15:42:42 +0200

    Create React App

---
+++ part1/part1 - React/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Mensaje from './Mensaje';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Mensaje color="red" message= "Estamos trabajando" />
+      <Mensaje color="blue" message= "en un curso" />
+      <Mensaje color="green" message= "de React" />
+    </div>
+  )
+}
+
+export default App;
